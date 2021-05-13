bf498be63938abf6eb67df23f73bda82b69919ac
S-Reiji <c0118136e0@edu.teu.ac.jp>
2021-05-13 23:27:20 +0900

    change getenv arg

--- src/main/java/sql/SQL.java
+++ src/main/java/sql/SQL.java
@@ -1,18 +1,10 @@
 package sql;
 
 import java.sql.*;
-import java.net.*;
 
 public abstract class SQL {
-    private static final String DB_SCHEME = "jdbc:postgresql://";
-    
     protected static Connection getCon() throws Exception {
-        URI dbURI = new URI(System.getenv("DATABASE_URL"));
-        return DriverManager.getConnection(
-            DB_SCHEME + dbURI.getHost() + dbURI.getPath() + ":" + dbURI.getPort(),
-            dbURI.getUserInfo().split(":")[0],
-            dbURI.getUserInfo().split(":")[1]
-        );
+        return DriverManager.getConnection(System.getenv("JDBC_DATABASE_URL"));
     }
     
     protected static Statement getStmt() throws Exception {
