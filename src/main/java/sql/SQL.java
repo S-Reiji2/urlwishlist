e0676c472261233eb7c0d575b11aa18b87254d77
S-Reiji <c0118136e0@edu.teu.ac.jp>
2021-05-13 22:28:41 +0900

    rename psql to sql

--- src/main/java/sql/SQL.java
+++ src/main/java/sql/SQL.java
@@ -1,23 +1,25 @@
 package sql;
 
 import java.sql.*;
+import java.net.*;
 
 public abstract class SQL {
-    protected static final
-            String DB_NAME = "game_db",
-            USER = "APP",
-            PASS = "app",
-            URL = "jdbc:derby://localhost/" + DB_NAME;
-
+    private static final String DB_SCHEME = "jdbc:postgresql://";
+    
+    protected static Connection getCon() throws Exception {
+        URI dbURI = new URI(System.getenv("DATABASE_URL"));
+        return DriverManager.getConnection(
+            DB_SCHEME + dbURI.getHost() + dbURI.getPath() + ":" + dbURI.getPort(),
+            dbURI.getUserInfo().split(":")[0],
+            dbURI.getUserInfo().split(":")[1]
+        );
+    }
+    
     protected static Statement getStmt() throws Exception {
-        return DriverManager
-                .getConnection(URL, USER, PASS)
-                .createStatement();
+        return getCon().createStatement();
     }
     
     protected static PreparedStatement getPreStmt(String sql) throws Exception {
-        return DriverManager
-                .getConnection(URL, USER, PASS)
-                .prepareStatement(sql);
+        return getCon().prepareStatement(sql);
     }
 }
